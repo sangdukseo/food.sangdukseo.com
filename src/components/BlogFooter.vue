07bb11e6a0ed2d339851137cb3c4c8bb3d692d18
Sang Seo <email@sangdukseo.com>
2019-04-10 00:51:28 -0400

    Update BlogFooter.vue

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        Proudly powered by <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a>
+        <a href="https://cosmicjs.com/add-bucket?import_bucket=5cad6ea5e224484fdc1c9d6f"><img src="https://s3-us-west-2.amazonaws.com/cosmicjs/51fe54d0-4f6e-11e9-9f32-8d001da69630-powered-by-cosmicjs.svg" /></a>
       </div>
     </footer>
   </transition>
