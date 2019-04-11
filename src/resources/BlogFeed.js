4657b7d0a23484afbd20ac0ae6d28859cd7e4832
Sang Seo <email@sangdukseo.com>
2019-04-11 01:11:06 -0400

    Update BlogFeed.js

--- src/resources/BlogFeed.js
+++ src/resources/BlogFeed.js
@@ -8,7 +8,7 @@ export default {
         let _posts = _map(response.objects, function(i) {
           let temp = {
             title: i.title,
-            image: `https://drive.google.com/uc?export=view&id=${i.metadata.drivekey.key}`,
+            image: `https://drive.google.com/uc?export=view&id=${i.metadata.drivekey}`,
             published: i.created_at,
             author: i.metadata.author.title,
             id: i.slug
